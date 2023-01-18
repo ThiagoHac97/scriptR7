0d723ffeb1060a612f3e2b11fac75cc127a1fd9a
Thiago Henrique <56203523+ThiagoHac97@users.noreply.github.com>
2023-01-18 15:14:07 -0300

    Add files via upload

---
+++ instant_article.js
@@ -0,0 +1,8 @@
+var googletag = window.googletag || {cmd: []};
+googletag.cmd.push(function() {
+    googletag.defineSlot('/7542,40069432/parceiros/bebemamae', [[320,50]], 'facebook_banner')
+          .addService(googletag.pubads())
+          .setTargeting('pos', 'facebook-banner-mobile-diaria');
+    googletag.enableServices();
+    googletag.display('facebook_banner')
+});
